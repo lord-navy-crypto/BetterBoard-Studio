72259248b92bf19853c377b5e3f012edf39a2d30
lord-navy-crypto <haojunli283@gmail.com>
2026-09-12 17:42:30 +0800

    cpp-core: add periodic sampler

---
+++ firmware/betterboard-core/src/core/PeriodicSampler.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace betterboard {
+namespace core {
+
+class PeriodicSampler {
+public:
+    explicit PeriodicSampler(uint32_t period_us = 10000U);
+
+    void setPeriodMicros(uint32_t period_us);
+    uint32_t periodMicros() const;
+    void reset(uint32_t now_us = 0U);
+    bool ready(uint32_t now_us);
+
+private:
+    uint32_t period_us_;
+    uint32_t next_due_us_;
+    bool armed_;
+};
+
+}  // namespace core
+}  // namespace betterboard
