356445d9048eec679e562800585cb1c768cbcf56
lord-navy-crypto <haojunli283@gmail.com>
2026-09-12 17:42:20 +0800

    cpp-core: add umbrella header

---
+++ firmware/betterboard-core/src/BetterBoard.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "core/PeriodicSampler.h"
+#include "math/FiniteDifference.h"
+#include "math/OnlineStatistics.h"
+#include "math/TrapezoidIntegrator.h"
+#include "measurement/MeasurementQuality.h"
+#include "signal/ExponentialMovingAverage.h"
+#include "signal/PeakHold.h"
